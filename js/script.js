d26a1b5917a56c72d51087c8db71b1e7624d027d
Rodrigo <rodrigorael53@gmail.com>
2022-07-03 18:48:37 -0300

    doing the logic of the contact of mario with the pipe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
   // adicionando uma nova classe
@@ -8,5 +9,28 @@ const jump = () => {
     mario.classList.remove('jump');
   }, 500); //500ms
 };
+
+const loop = setInterval(() => {
+  
+  // acessando o deslocamento esquerdo do pipe
+  const pipePosition = pipe.offsetLeft;
+  // o + serve para converter para um numero
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) { // 120px 80px
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './images/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+  }
+
+}, 10); // 10ms
 
 document.addEventListener('keydown', jump);
